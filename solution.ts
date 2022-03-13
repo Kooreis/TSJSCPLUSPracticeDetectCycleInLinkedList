13bcd3b945a6ab30f23075788851286665b2697d
Kooreis <kooreis.official@gmail.com>
2022-03-13 18:06:53 -0500

    This update introduces the LinkedList class. It has a head property and an append method. The append method is used to add new nodes to the linked list.

--- solution.ts
+++ solution.ts
@@ -1,9 +1,21 @@
-class Node {
-    value: any;
-    next: Node | null;
+class LinkedList {
+    head: Node | null;
 
-    constructor(value: any, next: Node | null = null) {
-        this.value = value;
-        this.next = next;
+    constructor() {
+        this.head = null;
+    }
+
+    append(value: any) {
+        if (!this.head) {
+            this.head = new Node(value);
+            return;
+        }
+
+        let current = this.head;
+        while (current.next) {
+            current = current.next;
+        }
+
+        current.next = new Node(value);
     }
 }
